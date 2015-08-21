20c2c6e7b665ca223a8891b8bfc0c6b38ebcee78
Bastien Falcou <bastien@fueled.com>
2015-08-21 11:25:23 -0400

    feat(PageControlTests): improved file and added tests.

--- EthanolUIComponentsTests/PageControlTests.m
+++ EthanolUIComponentsTests/PageControlTests.m
@@ -14,6 +14,9 @@
 
 @property (nonatomic, strong) NSMutableArray *dotsArray;
 
+- (CGFloat)xOriginForDotAtIndex:(NSInteger)dotIndex;
+- (void)didTapDot:(id)sender;
+
 @end
 
 @interface PageControlTests : XCTestCase
@@ -42,6 +45,16 @@
   self.activeRightImage = [UIImage imageNamed:@"Carousel-Middle-Active" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
 }
 
+#pragma mark - Test Load View
+
+- (void)testLoadPageControlInView {
+  UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Tests" bundle:[NSBundle bundleForClass:[self class]]];
+  UIViewController *testViewController = [storyboard instantiateViewControllerWithIdentifier:@"TestsViewControllerID"];
+  [testViewController loadView];
+  
+  XCTAssertNotNil(testViewController.view);
+}
+
 #pragma mark - Default Dots
 
 - (void)testPageControlNumberOfPages {
@@ -60,6 +73,22 @@
   XCTAssertEqual(pageControl.dotsArray.count, 3);
 }
 
+- (void)testPageControlNegativeNumberOfPages {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = -1;
+  
+  XCTAssertEqual(pageControl.dotsArray.count, 0);
+}
+
+- (void)testPageControlNumberOfPagesInferiorToCurrentPage {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 4;
+  pageControl.numberOfPages = 2;
+  
+  XCTAssertEqual(pageControl.currentPage, 1);
+}
+
 - (void)testPageControlDefaultCurrentPage {
   ETHPageControl *pageControl = [[ETHPageControl alloc] init];
   pageControl.numberOfPages = 5;
@@ -73,6 +102,22 @@
   pageControl.currentPage = 3;
   
   XCTAssertEqualObjects([(UIView *)pageControl.dotsArray[3] backgroundColor], pageControl.currentPageIndicatorTintColor);
+}
+
+- (void)testPageControlNegativeCurrentPage {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = -1;
+  
+  XCTAssertEqual(pageControl.currentPage, 0);
+}
+
+- (void)testPageControlCurrentPageSuperiorToNumberOfPages {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 10;
+  
+  XCTAssertEqual(pageControl.currentPage, pageControl.numberOfPages - 1);
 }
 
 - (void)testPageControlChangeOtherPagesColor {
@@ -97,7 +142,23 @@
   XCTAssert([pageControl.dotsArray[0] frame].origin.x > previousOriginX);
 }
 
-#pragma mark - View Changes
+- (void)testXOriginForDotAtIndexPathValue {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 200.0f, 100.0f)];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  
+  XCTAssertEqual([pageControl xOriginForDotAtIndex:2], pageControl.frame.size.width / 2.0f - [pageControl.dotsArray[2] frame].size.width / 2.0f);
+}
+
+- (void)testXOriginForDotAtIndexPathNegativeValue {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 200.0f, 100.0f)];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  
+  XCTAssertEqual([pageControl xOriginForDotAtIndex:-1], INFINITY);
+}
+
+#pragma mark - View Changes And Sizes
 
 - (void)testPageControlSizeToFit {
   ETHPageControl *pageControl = [[ETHPageControl alloc] init];
@@ -132,6 +193,51 @@
   [pageControl layoutIfNeeded];
   
   XCTAssert([pageControl.dotsArray[0] frame].origin.x > previousOriginX);
+}
+
+- (void)testPageControlSizeForNumberOfPagesNoPages {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 0;
+  [pageControl sizeToFit];
+  
+  XCTAssertEqual(pageControl.frame.size.width, 0.0f);
+  XCTAssertEqual(pageControl.frame.size.height, 0.0f);
+}
+
+- (void)testPageControlSizeForNumberOfPagesOnePage {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 1;
+  [pageControl sizeToFit];
+  
+  XCTAssertEqual(pageControl.frame.size.width, [pageControl.dotsArray[0] frame].size.width);
+  XCTAssertEqual(pageControl.frame.size.height, [pageControl.dotsArray[0] frame].size.height);
+}
+
+- (void)testPageControlSizeForNumberOfPagesTwoPages {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 2;
+  [pageControl sizeToFit];
+  
+  XCTAssertEqual(pageControl.frame.size.width, [pageControl.dotsArray[0] frame].size.width * 2.0f + pageControl.dotsSpace);
+  XCTAssertEqual(pageControl.frame.size.height, [pageControl.dotsArray[0] frame].size.height);
+}
+
+- (void)testPageControlSizeForNumberOfPagesTenPages {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 10;
+  [pageControl sizeToFit];
+  
+  XCTAssertEqual(pageControl.frame.size.width, [pageControl sizeForNumberOfPages:10].width);
+  XCTAssertEqual(pageControl.frame.size.height, [pageControl sizeForNumberOfPages:10].height);
+}
+
+- (void)testDistanceBetweenDotsChange {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 2;
+  pageControl.dotsSpace = 10.0f;
+  [pageControl sizeToFit];
+
+  XCTAssertEqual(pageControl.frame.size.width, [pageControl.dotsArray[0] frame].size.width + 10.0f + [pageControl.dotsArray[1] frame].size.width);
 }
 
 #pragma mark - Custom Dot Images
@@ -180,7 +286,7 @@
   }
 }
 
-- (void)testPageControlCustomImagesInactiveLeft {
+- (void)testPageControlCustomImagesActiveLeft {
   ETHPageControl *pageControl = [[ETHPageControl alloc] init];
   pageControl.numberOfPages = 5;
   pageControl.currentPage = 0;
@@ -192,7 +298,7 @@
   XCTAssertEqualObjects([pageControl.dotsArray[0] image], self.inactiveLeftImage);
 }
 
-- (void)testPageControlCustomImagesInactiveLeftOthersDifferent {
+- (void)testPageControlCustomImagesActiveLeftOthersDifferent {
   ETHPageControl *pageControl = [[ETHPageControl alloc] init];
   pageControl.numberOfPages = 5;
   pageControl.currentPage = 4;
@@ -230,6 +336,7 @@
 - (void)testPageControlCustomImagesInactiveMiddleOthersDifferent {
   ETHPageControl *pageControl = [[ETHPageControl alloc] init];
   pageControl.numberOfPages = 5;
+  pageControl.currentPage = 4; // Arbitrary value
   
   pageControl.leftDotImageActive = self.inactiveLeftImage;
   pageControl.middleDotImageActive = self.inactiveMiddleImage;
@@ -239,8 +346,35 @@
   pageControl.middleDotImageInactive = self.inactiveMiddleImage;
   pageControl.rightDotImageInactive = self.inactiveRightImage;
   
-  XCTAssertNotEqualObjects([[pageControl.dotsArray firstObject] image], self.inactiveMiddleImage);
-  XCTAssertNotEqualObjects([[pageControl.dotsArray lastObject] image], self.inactiveMiddleImage);
+  UIImage *imageLeft = (UIImage *)[pageControl.dotsArray[0] image];
+  UIImage *imageRight = (UIImage *)[pageControl.dotsArray[4] image];
+  
+  XCTAssert(imageLeft != self.inactiveMiddleImage);
+  XCTAssert(imageRight != self.inactiveMiddleImage);
+}
+
+- (void)testPageControlMiddleDotImageActiveOnePage {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 1;
+  pageControl.currentPage = 0;
+  
+  pageControl.middleDotImageActive = self.activeMiddleImage;
+  
+  UIImage *imageDot = (UIImage *)[pageControl.dotsArray[0] image];
+  
+  XCTAssertEqualObjects(imageDot, self.activeMiddleImage);
+}
+
+- (void)testPageControlMiddleImageActive {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 2;
+  
+  pageControl.middleDotImageActive = self.activeMiddleImage;
+  
+  UIImage *imageDot = (UIImage *)[pageControl.dotsArray[2] image];
+  
+  XCTAssertEqualObjects(imageDot, self.activeMiddleImage);
 }
 
 #pragma mark - Properties
@@ -257,8 +391,74 @@
   ETHPageControl *pageControl = [[ETHPageControl alloc] init];
   pageControl.numberOfPages = 1;
   pageControl.hidesForSinglePage = YES;
+  pageControl.numberOfPages = 1;
   
   XCTAssertEqual(pageControl.dotsArray.count, 0);
+}
+
+- (void)testPageControlDefersCurrentPageDisplayBeforeUpdate {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  pageControl.defersCurrentPageDisplay = YES;
+  pageControl.currentPage = 4;
+  
+  XCTAssertEqualObjects([pageControl.dotsArray[4] backgroundColor], pageControl.pageIndicatorTintColor);
+}
+
+- (void)testPageControlDefersCurrentPageDisplayAfterUpdate {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  pageControl.defersCurrentPageDisplay = YES;
+  pageControl.currentPage = 4;
+  [pageControl updateCurrentPageDisplay];
+  
+  XCTAssertEqualObjects([pageControl.dotsArray[4] backgroundColor], pageControl.currentPageIndicatorTintColor);
+}
+
+- (void)testPageControlResetDotsTintColor {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 4;
+  pageControl.pageIndicatorTintColor = [UIColor redColor];
+  pageControl.pageIndicatorTintColor = nil;
+  
+  XCTAssertEqualObjects([pageControl.dotsArray[0] backgroundColor], [[UIColor whiteColor] colorWithAlphaComponent:0.5f]);
+}
+
+- (void)testPageControlResetCurrentDotTintColor {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+  pageControl.currentPageIndicatorTintColor = nil;
+  
+  XCTAssertEqualObjects([pageControl.dotsArray[0] backgroundColor], [UIColor whiteColor]);
+}
+
+#pragma mark - Actions
+
+- (void)testPageControlTapDot {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  
+  UIView *dotView = pageControl.dotsArray[2];
+  [pageControl didTapDot:dotView.gestureRecognizers[0]];
+
+  XCTAssertEqual(pageControl.currentPage, 2);
+}
+
+- (void)testPageControlTapDotInvalidType {
+  ETHPageControl *pageControl = [[ETHPageControl alloc] init];
+  pageControl.numberOfPages = 5;
+  pageControl.currentPage = 0;
+  
+  UIView *dotView = pageControl.dotsArray[2];
+  [pageControl didTapDot:dotView];
+  
+  XCTAssertNotEqual(pageControl.currentPage, 2);
 }
 
 @end
